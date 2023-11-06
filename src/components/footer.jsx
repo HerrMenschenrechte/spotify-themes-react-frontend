4984c3be7ce256915c57c9c7b29c340009706ab7
Cedric Mensah <cedric@mensah.de>
2023-11-06 10:02:49 +0100

    Created footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+
+
+function Footer() {
+   return (
+<footer className="bg-dark">
+    <div className="row">
+        <div className="col-md">
+            <a className="nav-link" href="cookies"></a>
+        </div>
+            
+        <div className="col-md">
+            <a className="nav-link" href="mailto:cmensahde@gmail.com"> Contact</a>
+        </div>
+           
+        <div className="col-md">
+            <span className="nav-link"> © 2023 Cedric Mensah</span>
+        </div>
+        
+        </div> 
+</footer>
+   )
+}
+
+
+export default Footer;
